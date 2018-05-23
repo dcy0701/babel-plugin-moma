1ef817d4c70fe77c92e8f403e753cedd9a481974
dongchongyang <dongchongyang@meituan.com>
2018-05-23 09:14:33 +0800

    release: release 1.0.5

--- src/core.js
+++ src/core.js
@@ -1,28 +1,35 @@
 module.exports = function core(defaultLibraryName) {
-  return ({ types }) => {
-      return {
-          visitor: {
-              ImportDeclaration(path, _ref = {opts:{}}){
-                  const specifiers = path.node.specifiers
-                  const source = path.node.source
+  return ({ types }) => ({
+    visitor: {
+      ImportDeclaration(path) {
+        const specifiers = path.node.specifiers;
+        const source = path.node.source;
 
-                  if (source.value !== defaultLibraryName
-                      && source.value.indexOf(defaultLibraryName + '/merchant') === -1
-                      && source.value.indexOf(defaultLibraryName + '/dp') === -1) {
-                          return
-                  }
+        if ([defaultLibraryName, defaultLibraryName + '/merchant',
+                defaultLibraryName + '/dp'].indexOf(source.value) === -1) {
+          return;
+        }
 
-                  if (!types.isImportDefaultSpecifier(specifiers[0]) ) {
-                      const declarations = specifiers.map((specifier) => {
-                          return types.ImportDeclaration(
-                              [types.importDefaultSpecifier(specifier.local)],
-                              types.StringLiteral(`${source.value}/${specifier.local.name}`)
-                          )
-                      })
-                      path.replaceWithMultiple(declarations)
-                  }
-              }
-          }
-      }
-  }
-}
+        let themeName = '';
+
+        switch (source.value) {
+          case '@dp/moma-ui/merchant':
+            themeName = '/merchant';
+            break;
+          case '@dp/moma-ui/dp':
+            themeName = '/dp';
+            break;
+          default:
+            themeName = '';
+        }
+
+        if (!types.isImportDefaultSpecifier(specifiers[0])) {
+          const declarations = specifiers.map((specifier) =>
+        types.ImportDeclaration([types.importDefaultSpecifier(specifier.local)],
+          types.StringLiteral(defaultLibraryName + '/' + specifier.local.name + themeName)));
+          path.replaceWithMultiple(declarations);
+        }
+      },
+    },
+  });
+};
--- test/index.js
+++ test/index.js
@@ -6,13 +6,8 @@ const moma = require('./../src/index.js')
 const visitor = moma({types});
 
 const code = `
-import Toast from '@dp/moma-ui/Toast/merchant';
-import Input from '@dp/moma-ui/Input/merchant';
-import Button from '@dp/moma-ui/Button/merchant';
-import Confirm from '@dp/moma-ui/Confirm/merchant';
-import Loading from '@dp/moma-ui/Loading/merchant';
-import Checkbox from '@dp/moma-ui/Checkbox/merchant';
-import { ModelComponent } from '@dp/moma-ui/Model/merchant';`
+import { Radio, Toast, Button, Loading } from '@dp/moma-ui/merchant'
+`
 
 const result = babel.transform(code, {
     plugins: [visitor]
